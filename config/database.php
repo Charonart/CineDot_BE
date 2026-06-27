e74505ce768b8bf5429fb9d394ae10230052fcca
LE BA QUY <Lequy27102006@gmail.com>
2026-06-27 18:02:31 +0700

    Add Heroku deploy files (Procfile, nginx.conf, Redis SSL config) to main

--- config/database.php
+++ config/database.php
@@ -133,6 +133,10 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
         ],
 
         'cache' => [
@@ -142,6 +146,10 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
         ],
 
     ],
